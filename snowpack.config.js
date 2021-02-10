050e16b2792f79d2ff00627539ae171f546033fe
iemong <kiyoshidainagon@gmail.com>
2021-02-10 09:14:24 +0900

    typescriptのpluginを追加

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     public: "/",
     src: "/dist",
   },
-  plugins: ["@snowpack/plugin-react-refresh"],
+  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-typescript"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
