ddb189ddae5eb2eaad1345ff3a6694884ff8a06b
iemong <kiyoshidainagon@gmail.com>
2021-02-11 23:40:08 +0900

    spaの時のフォールバックを追加

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,13 +1,13 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: "/", static: true},
+    public: { url: "/", static: true },
     src: "/dist",
   },
   plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-typescript"],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: "routes", src: ".*", dest: "/index.html" },
   ],
   optimize: {
     /* Example: Bundle your final build: */
