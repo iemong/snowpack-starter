59ead180dbf418fd54df1a52b0b3dffa7a5bfabe
iemong <kiyoshidainagon@gmail.com>
2021-02-12 22:08:23 +0900

    postcss.config.jsのrequireの書き方を変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
+const tailwindcss = require("tailwindcss");
+const postcssPresetEnv = require("postcss-preset-env");
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    tailwindcss(),
+    postcssPresetEnv({ browsers: "last 2 versions", stage: 1 }),
+  ],
 };
