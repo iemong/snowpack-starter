e9f74cf33e479590f957e4e633ebb8f651b77c13
iemong <kiyoshidainagon@gmail.com>
2021-02-15 23:25:31 +0900

    tailwind.config.jsをかく

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Nunito Sans", "Helvetica Neue", "Helvetica", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
