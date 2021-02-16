2efc0ab380dac3c01ec5b76df33765bbbdaf1ca8
iemong <kiyoshidainagon@gmail.com>
2021-02-16 09:28:22 +0900

    purge option を設定する

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    layers: ["components", "utilities"],
+    content: ["./src/**/*.tsx"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
